e5307029f7407e32c28e4457cf12e73c1fbc8203
suwandanurhali <60819992+suwandanurhali@users.noreply.github.com>
2020-02-08 23:07:42 +0700

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1MoER5hEe8e1i4i2JuPR0HJ3ot9KEyJTUp6wGTRXURb8/edit#gid=0';
